2973ed59388daf83dd59e109b26066a11a3f4357
Paritosh <ostudy7@gmail.com>
2018-11-20 20:50:48 -0500

    First Commit

---
+++ js/index.js
@@ -0,0 +1,34 @@
+
+document.addEventListener('deviceready', onDeviceReady, false); 
+function onDeviceReady(){
+let firstname = document.getElementById('firstname').value;
+let lastname = document.getElementById('lastname').value;
+let email = document.getElementById('email').value;
+let password = document.getElementById('password').value;
+let major = document.getElementById('major').value;
+let level = document.getElementById('select-native-1').value;
+let register = document.getElementById('registerbutton');
+
+register.addEventListener('click', function(e){
+    alert("Wat the fuck!");
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+//as soon as the window is loaded and ready and we need to change this after we click something
+
+
+
+  
